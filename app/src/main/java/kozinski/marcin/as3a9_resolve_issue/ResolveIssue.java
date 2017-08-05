245757f70cf122a1a7701268cd1512bb263a7176
Marcin Koziński <marcin.m.kozinski@gmail.com>
2017-08-05 07:42:45 +0200

    Add minimal repro case for resolve issue in AS 3.0 Canary 9

---
+++ app/src/main/java/kozinski/marcin/as3a9_resolve_issue/ResolveIssue.java
@@ -0,0 +1,9 @@
+package kozinski.marcin.as3a9_resolve_issue;
+
+import com.google.gson.Gson;
+
+public class ResolveIssue {
+	public static void fail() {
+		new Gson().toJson("y u no resolve");
+	}
+}
